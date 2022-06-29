eff0d71a4eca74d142156dd58819d99b776b6d98
Marina <gtellier.mecanics@gmail.com>
2022-06-29 16:03:44 +0300

    Программа для определения выходного

---
+++ task15/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Нет, это Понедельник!");
+}
+
+if (number == 2)
+{
+    Console.WriteLine("Нет, это Вторник!");
+}
+
+if (number == 3)
+{
+    Console.WriteLine("Нет, это Среда!");
+}
+
+if (number == 4)
+{
+    Console.WriteLine("Нет, это Четверг!");
+}
+
+if (number == 5)
+{
+    Console.WriteLine("Нет, это Пятница!");
+}
+
+if (number == 6)
+{
+    Console.WriteLine("Да, это Суббота!");
+}
+
+if (number == 7)
+{
+    Console.WriteLine("Да, это Воскресенье!");
+}
