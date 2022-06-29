6d32e0dd7cbba666b63d20fd6b06a5335a9bfbb4
Marina <gtellier.mecanics@gmail.com>
2022-06-29 15:52:55 +0300

    Программа, которая выводит третью цифру числа

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 100 == number)
+{
+    Console.WriteLine("Нет третьего числа");
+}
+else
+{
+    Console.WriteLine(number % 10);
+}
